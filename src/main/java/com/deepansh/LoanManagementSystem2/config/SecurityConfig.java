cd5a1895adefc5c086fdb95fe26f11d1d97c22e2
Deepansh Srivastava <deepharshsri.98@gmail.com>
2026-05-03 12:19:33 +0530

    actuator added

--- src/main/java/com/deepansh/LoanManagementSystem2/config/SecurityConfig.java
+++ src/main/java/com/deepansh/LoanManagementSystem2/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
           .requestMatchers(HttpMethod.POST,"/api/auth/login").permitAll()
           .requestMatchers(HttpMethod.POST,"/api/auth/register").permitAll()
           .requestMatchers(HttpMethod.POST,"/api/otp/send").permitAll()
+          .requestMatchers("/actuator/**").permitAll()
           .anyRequest().permitAll())
           .csrf(csrf->csrf.disable())
           .sessionManagement(sesssion->sesssion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
